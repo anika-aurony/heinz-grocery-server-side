6c99fce5c2d4a01872bda18248e51879b77eef0c
anikaaurony <aurony111@gmail.com>
2022-05-21 22:17:53 +0600

    delete  done

--- index.js
+++ index.js
@@ -42,7 +42,15 @@ async function run() {
             const result = await itemCollection.insertOne(newProduct);
             res.send(result);
 
-        })
+        });
+
+        //Delete
+        app.delete('/inventory/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
